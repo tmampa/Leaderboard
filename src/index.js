9e5d71ded7ace6e63745088c062280375fe56f53
tmampa <tmampa94@gmail.com>
2021-12-24 06:45:50 +0200

    Add load scores from api function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import './style.css';
 
-const board = document.getElementById('board');
+const sendData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
 
-const myScores = [{
-  name: 'Sean',
-  score: 100,
-},
-{
-  name: 'Dave',
-  score: 100,
-},
-{
-  name: 'Tshephang',
-  score: 100,
-}];
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+const submitButton = document.getElementById('submit');
 
-const displayScores = () => {
-  board.innerHTML = '';
-  myScores.forEach((myScore) => {
-    const content = `${myScore.name}: ${myScore.score}`;
+submitButton.addEventListener('click', async () => {
+  await sendData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3qxAA7wy5DPzg65q4nWs/scores/', { user: nameInput.value, score: +scoreInput.value });
+  nameInput.value = '';
+  scoreInput.value = '';
+});
 
-    const listItem = document.createElement('li');
-    listItem.innerText = content;
-    listItem.className = 'list-item';
-    board.appendChild(listItem);
+const refreshButton = document.getElementById('refresh');
+const scoresUl = document.getElementsByClassName('boardlist')[0];
+
+const loadScores = async () => {
+  scoresUl.innerHTML = '';
+  const { result: scores } = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3qxAA7wy5DPzg65q4nWs/scores/').then((response) => response.json());
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    scoresUl.appendChild(li);
   });
 };
 
-window.addEventListener('load', () => {
-  displayScores();
-});
+refreshButton.addEventListener('click', loadScores);
+window.onload = loadScores;
